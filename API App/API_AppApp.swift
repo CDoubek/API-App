453e6c358a879ff752c49ce6a8fd05cf995f40ed
Colin Doubek <doubekcolin@gmail.org>
2021-02-25 10:17:21 -0600

    Initial Commit

---
+++ API App/API_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_AppApp.swift
+//  API App
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct API_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
